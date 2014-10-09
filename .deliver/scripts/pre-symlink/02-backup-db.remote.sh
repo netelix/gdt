81bba5155f17ab1d4a16c94df8316fab2abf6f40
alex <netelix@gmail.com>
2014-10-09 10:39:23 -0400

    bug nofile

--- .deliver/scripts/pre-symlink/02-backup-db.remote.sh
+++ .deliver/scripts/pre-symlink/02-backup-db.remote.sh
@@ -1,3 +1,3 @@
 
 TARGET_DB=$(cat "${DELIVERY_PATH}/.target-db")
-mysqldump --add-locks --add-drop-table --extended-insert --lock-tables --ignore-table="{TARGET_DB}".apache_logs -u root "${TARGET_DB}" > "${REMOTE_PATH}/deliver-backup.sql"
+mysqldump --add-locks --add-drop-table --extended-insert --lock-tables -u root "${TARGET_DB}" > "${REMOTE_PATH}/deliver-backup.sql"
