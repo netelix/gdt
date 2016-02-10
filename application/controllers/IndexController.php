8e470b901c630a88a49041db43d2a6532a65a2d4
Alexis <netelix@gmail.com>
2016-02-10 10:57:29 +0100

    recette

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -9,6 +9,26 @@ class IndexController extends Uop_Controller_Index
   }
   
   public function creationAction()
-  {}
+  {
+	  $form = App::form("Simple");
+	  $form->addText("email", array("required"=>true));
+		$this->view->contacted = $this->_getParam("submitted");
+
+		if( $this->_isSubmittedAndValid($form)){
+	 		$z_mail = new Zend_Mail("UTF-8");
+    	foreach(App::table("users")->select()->where("type='admin'")->fetchAll() as $r_user){
+	    	$z_mail->addTo($r_user->email);
+	    }
+    	$z_mail->setSubject("Lead creation");
+    	$txt = "";
+    	foreach($form->getValues() as $key=>$value){
+	    	$txt.="$key : $value\n";
+    	}
+    	$z_mail->setBodyText($txt);
+    	$z_mail->send();
+    	$this->_gotoUrl($this->getRequest()->getHeader("referer").'?submitted=1');
+		}
+		$this->view->form = $form;
+  }
 }
 
--- application/controllers/LocationController.php
+++ application/controllers/LocationController.php
@@ -39,7 +39,26 @@ class LocationController extends Uop_Controller_Location
   
   public function conventionsAction()
   {
-	  
+	  	  $form = App::form("Simple");
+		  $form->addText("email", array("required"=>true));
+			$this->view->contacted = $this->_getParam("submitted");
+	
+			if( $this->_isSubmittedAndValid($form)){
+		 		$z_mail = new Zend_Mail("UTF-8");
+	    	foreach(App::table("users")->select()->where("type='admin'")->fetchAll() as $r_user){
+		    	$z_mail->addTo($r_user->email);
+		    }
+	    	$z_mail->setSubject("Lead conventions");
+	    	$txt = "";
+	    	foreach($form->getValues() as $key=>$value){
+		    	$txt.="$key : $value\n";
+	    	}
+	    	$z_mail->setBodyText($txt);
+	    	$z_mail->send();
+	    	$this->_gotoUrl($this->getRequest()->getHeader("referer").'?submitted=1');
+			}
+		$this->view->form = $form;
+
   }
   
   protected function _filterTypes()
