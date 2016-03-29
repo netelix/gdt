3de2ab0d27bbc47eb74baf335bc6ca221b1580bb
Alexis <netelix@gmail.com>
2016-03-29 11:02:36 +0200

    images push

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -30,5 +30,58 @@ class IndexController extends Uop_Controller_Index
 		}
 		$this->view->form = $form;
   }
+  
+  public function untattooAction()
+  {
+	  $form = App::form("Simple");
+	  $form->addText("email", array("required"=>true));
+	  $adm2 = App::table("locations")->country("FR")->getDescendants(null, array("type"=>"adm2"));
+	  $adm2_options = array(__("Votre département"));
+	  foreach($adm2 as $r_loc){
+	  	if($r_loc->name()->__toString()){
+ 			  $adm2_options[$r_loc->name()->__toString()] = $r_loc->name()->__toString();
+	  	}
+	  }
+	  ksort($adm2_options);
+	  $form->addSelect("adm2", array("multiOptions"=>$adm2_options));
+	  $element = new Zend_Form_Element_File('image');
+
+	  $element->addValidator('Extension', false, 'jpg,png,gif')
+	  	->setDestination(APPLICATION_PATH.'/../tmp');
+	  $form->addElement($element, 'image');
+		$this->view->contacted = $this->_getParam("submitted");
+
+		if( $this->_isSubmittedAndValid($form)){
+			$location = $form->image->getFileName();
+	 		$z_mail = new Zend_Mail("UTF-8");
+    	foreach(App::table("users")->select()->where("type='admin'")->fetchAll() as $r_user){
+	    	$z_mail->addTo($r_user->email);
+	    }
+	    
+    	$z_mail->setSubject("Lead creation");
+    	$txt = "";
+    	foreach($form->getValues() as $key=>$value){
+	    	$txt.="$key : $value\n";
+    	}
+    	$z_mail->setBodyText($txt);
+
+			if(!empty($location)){
+	 			if (!$form->image->receive()) {
+			    print "Erreur de réception de fichier";
+				}
+	 				
+				$at = $z_mail->createAttachment(file_get_contents($location));
+				$at->type        = 'image/gif';
+				$at->disposition = Zend_Mime::DISPOSITION_ATTACHMENT;
+				$at->encoding    = Zend_Mime::ENCODING_BASE64;
+				$at->filename    = $form->image;
+			}
+
+    	$z_mail->send();
+    	$this->_gotoUrl($this->getRequest()->getHeader("referer").'?submitted=1');
+		}
+		$this->view->form = $form;
+  }
+
 }
 
--- application/controllers/OrganizationController.php
+++ application/controllers/OrganizationController.php
@@ -54,7 +54,130 @@ class OrganizationController extends Uop_Controller_Organization
 			->where("org_id IS NULL")
 			->fetchRow() !== NULL;
 	}
-}
 
+  protected function manageImageUpload($r_org)
+  {
+    $req = $this->getRequest();
+    switch ($this->getRequest()->getMethod())
+    {
+      case 'OPTIONS':
+      case 'HEAD':
+        $this->head();
+        break;
+      case 'GET':
+        if ($this->_isAjax())
+        {
+          $res = array();
+          foreach ($images as $image)
+          {
+            $res[] = $this->jsonify($image);
+          }
+
+          $this->_json(array(
+            'files' => $res
+          ));
+        }
+        break;
+      case 'PATCH':
+      case 'PUT':
+      case 'POST':
+        $adapter = new Zend_File_Transfer_Adapter_Http();
+        $titles  = $req->getParam('title');
+
+        $adapter->addValidator('Extension', false, 'jpg,png,gif,jpeg');
+
+        $files = $adapter->getFileInfo();
+        $res   = array();
+        $i = 0;
+        foreach ($files as $file => $info) {
+          $name  = basename($adapter->getFileName($file));
+          $image = App::table('images')->createRowFor($r_org);
+          $error = null;
+
+          try {
+            $image->mime($adapter->getMimeType($file));
+            // file uploaded & is valid
+            if (!$adapter->isUploaded($file))
+            {
+              throw new Exception('File not uploaded');
+            }
+
+            $image->import($info['tmp_name']);
+
+            if (!empty($titles) && isset($titles[$i]))
+            {
+              $image->name(basename($titles[$i]));
+            } else {
+            	$name = basename($name, ".".$image->format);
+              $image->name($name);
+            }
+
+            $image->buildBasePathIfNeeded();
+            $adapter->setDestination($image->basepath());
+
+            $image->updateThumbs(true);
+
+          } catch( Exception $e ){
+            $error = $e->getMessage();
+            if (!empty($image->id))
+            {
+              $image->delete();
+            }
+            if(APPLICATION_ENV != 'production'){
+              throw $e;
+            }
+          }
+          if(isset($error) && APPLICATION_ENV=='production'){
+            error_log($e);
+            $error = 'Une erreur interne est survenue lors du téléchargement. Veuillez nous en excuser.';
+          }
+          $res[] = $this->jsonify($image, $error);
+        }
+        if($r_org instanceOf Uop_Model_DbTable_Row_Organization){
+	    	  $r_org->republish();
+        }
+
+        $this->_json(array(
+          'files' => $res
+        ));
+        break;
+
+    case 'DELETE':
+      $table = App::table('images');
+      $row   = $this->_findResource(array('privilege'=>'delete', 'table' => 'images', 'id_field' => 'image_id'));
+      $row->delete();
+
+      $response = array('success' => true);
+      try {
+        $r_org->republish();
+      } catch(Exception $e){
+        $response["reload"] = true;
+      }
+
+      $this->_json($response);
+      break;
+    }
+  }
+  
+	
+  protected function _manageOrganizationDemand($form)
+  {
+    if($this->getRequest()->isXmlHttpRequest()){
+	    $this->manageImageUpload(App::table("messages")->createRow()->set("id",1));
+    }
+    if($this->_isSubmittedAndValid($form)){
+      $r_message = $form->org()->demand($form);
+      $t_images = App::table("images");
+      foreach($_POST["images"] as $img_id){
+	      if($r_image = $t_images->find($img_id)->current()){
+	      	$r_image->ref_type = "messages";
+	      	$r_image->ref_id = $r_message->id;
+	      	$r_image->save();
+	      }
+      }
+      $this->_reload();
+    }
+  }
+}
 
 
